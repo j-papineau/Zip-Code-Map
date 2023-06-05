a695d3467859ad310d4c0b90db14c3461b8b2229
Joel Papineau <118376438+j-papineau@users.noreply.github.com>
2023-06-05 11:39:16 -0400

    move to laptop

--- MAIN/js/zipmap.js
+++ MAIN/js/zipmap.js
@@ -1,10 +1,49 @@
+//classes
+
+const locationCircles = []
+
+class LocationCircle{
+
+   constructor(id, latlng){
+
+        this.id = id;
+        this.circle = L.circle(latlng,{
+
+            color:'blue',
+            fillColor: 'blue',
+            fillOpacity: .5,
+            radius: radiusKM
+
+        })
+        
+        this.marker = L.marker(e.latlng);
+   }
+// add to map method: Shawn
+   addToMap() {
+        
+   }
+
+   //getters
+
+   getMarker(){ return this.marker; }
+
+   getCircle(){ this.circle }
+
+   getId(){ this.id }
+
+}
+
+
+
 $(function(){
 
     let polyLine;
     let polygon;
-    var latlngs = [];
     var count = 1;
     var map = L.map('map').setView([35.31, -84.32], 4);
+    let circle;
+    let circleMarker;
+    var circleCount = 0;
 
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 17,
@@ -16,6 +55,48 @@ $(function(){
         //alert("You clicked the map at " + e.latlng);
         
        // addToLine(e.latlng);
+       // radiusCheck = $("#radiusCheck").val();
+
+       
+       //check map draw mode
+        if($('#radiusCheck').is(':checked')){
+
+                radiusKM = $("#radiusSize").val();
+                radiusKM = radiusKM * 1000; //convert m to km
+
+                console.log("drawing circle at " + e.latlng + " with radius size: " + radiusKM + "km");
+                //add marker on click
+                console.log(circleCount);
+            
+                circleCount = circleCount + 1;
+
+                
+                //Shawn Code
+                const newCircle = new LocationCircle(circleCount, e.latlng);
+
+
+                locationCircles.push(newCircle);
+
+                newCircle.addToMap(map);
+
+
+
+                // circle = L.circle(e.latlng,{
+
+                //     color:'blue',
+                //     fillColor: 'blue',
+                //     fillOpacity: .5,
+                //     radius: radiusKM
+
+                // }).addTo(map);
+                
+            
+
+            circleCount++;
+           // circle.redraw();
+        }
+
+        else{
         console.log(e.latlng);
 
         if(count == 1){
@@ -31,11 +112,15 @@ $(function(){
             polyLine.addLatLng([e.latlng.lat, e.latlng.lng]);
             count++;
         }
-
-        
+    }
     }
 
-   
+  
+    $("#clearCircle").click(function(){
+
+        circle.remove();
+
+    });
 
     $("#drawPolyBtn").click(function(){
 
@@ -78,3 +163,5 @@ $(function(){
     
 
     })//end ready
+
+   
