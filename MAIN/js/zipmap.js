4c562aaa95d45d13c199d97362debd30992388c4
Joel Papineau <118376438+j-papineau@users.noreply.github.com>
2023-06-05 14:50:27 -0400

    Added circle functions and ADR overlay

--- MAIN/js/zipmap.js
+++ MAIN/js/zipmap.js
@@ -1,167 +1,222 @@
 //classes
 
-const locationCircles = []
+// const locationCircles = []
 
-class LocationCircle{
+// class LocationCircle{
 
-   constructor(id, latlng){
+//    constructor(id, latlng){
 
-        this.id = id;
-        this.circle = L.circle(latlng,{
+//         this.id = id;
+//         this.circle = L.circle(latlng,{
 
-            color:'blue',
-            fillColor: 'blue',
-            fillOpacity: .5,
-            radius: radiusKM
+//             color:'blue',
+//             fillColor: 'blue',
+//             fillOpacity: .5,
+//             radius: radiusKM
 
-        })
-        
-        this.marker = L.marker(e.latlng);
-   }
-// add to map method: Shawn
-   addToMap() {
-        
-   }
+//         })
 
-   //getters
+//         this.marker = L.marker(latlng);
+//    }
+// // add to map method: Shawn
+//    addToMap(map) {
+//        //this.circle.addTo(map);
+//       // this.marker.addTo(map);
+//    }
 
-   getMarker(){ return this.marker; }
+//    //getters
 
-   getCircle(){ this.circle }
+//    getMarker(){ return this.marker; }
 
-   getId(){ this.id }
+//    getCircle(){ this.circle }
 
-}
+//    getId(){ this.id }
 
+// }
 
-
-$(function(){
-
-    let polyLine;
-    let polygon;
-    var count = 1;
-    var map = L.map('map').setView([35.31, -84.32], 4);
-    let circle;
-    let circleMarker;
-    var circleCount = 0;
-
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 17,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    }).addTo(map);
-
-
-    function onMapClick(e) {
-        //alert("You clicked the map at " + e.latlng);
-        
-       // addToLine(e.latlng);
-       // radiusCheck = $("#radiusCheck").val();
-
-       
-       //check map draw mode
-        if($('#radiusCheck').is(':checked')){
-
-                radiusKM = $("#radiusSize").val();
-                radiusKM = radiusKM * 1000; //convert m to km
-
-                console.log("drawing circle at " + e.latlng + " with radius size: " + radiusKM + "km");
-                //add marker on click
-                console.log(circleCount);
-            
-                circleCount = circleCount + 1;
-
-                
-                //Shawn Code
-                const newCircle = new LocationCircle(circleCount, e.latlng);
-
-
-                locationCircles.push(newCircle);
-
-                newCircle.addToMap(map);
-
-
-
-                // circle = L.circle(e.latlng,{
-
-                //     color:'blue',
-                //     fillColor: 'blue',
-                //     fillOpacity: .5,
-                //     radius: radiusKM
-
-                // }).addTo(map);
-                
-            
-
-            circleCount++;
-           // circle.redraw();
-        }
-
-        else{
-        console.log(e.latlng);
-
-        if(count == 1){
-            polyLine = L.polyline(
-                [
-                    [e.latlng.lat, e.latlng.lng],
-                    [e.latlng.lat, e.latlng.lng]
-                ]
-            ).addTo(map);
-            count++;
-        }else{
-            
-            polyLine.addLatLng([e.latlng.lat, e.latlng.lng]);
-            count++;
-        }
-    }
-    }
+//DOM READY
+$(function () {
+  //load kml
 
   
-    $("#clearCircle").click(function(){
 
-        circle.remove();
+  let polyLine;
+  let polygon;
+  var count = 1;
+  var map = L.map("map").setView([35.31, -84.32], 4);
+  var circleCount = 0;
+  let circles = [];
+  var track;
 
-    });
+  L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 17,
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  }).addTo(map);
 
-    $("#drawPolyBtn").click(function(){
+  //ADR map checkbox
 
-        coords = polyLine.getLatLngs();
-        console.log(coords);
+  $("#adrMapCheck").change(function(){
 
-        polygon = L.polygon(coords, {
-            //ill do this later lol 
-            color: 'red'
+    let isChecked = $("#adrMapCheck").prop("checked");
 
-        }).addTo(map);
+    console.log("Check box is now: " + isChecked);
 
-        setOutput();
+    if(isChecked){
+            fetch("assets/ADR-Map.kml")
+        .then((res) => res.text())
+        .then((kmltext) => {
+        // Create new kml overlay
+        const parser = new DOMParser();
+        const kml = parser.parseFromString(kmltext, "text/xml");
+        track = new L.KML(kml);
+        map.addLayer(track);
 
-    })
-
-    function setOutput(){
-        //setup input
-        let outputString;
-        outputString = JSON.stringify(polygon.getLatLngs());
-        console.log(JSON.parse(outputString));
-
-        $("#coordOutput").val(outputString);
+        });
+    }else{
+        map.removeLayer(track);
     }
+
+  })
+  
+  
+  
+  
+  
+  
+  
+  
+  //map click
+  function onMapClick(e) {
+    //alert("You clicked the map at " + e.latlng);
+
+    // addToLine(e.latlng);
+    // radiusCheck = $("#radiusCheck").val();
+
+    //check map draw mode
+    if ($("#radiusCheck").is(":checked")) {
+      radiusKM = $("#radiusSize").val();
+      radiusKM = radiusKM * 1000; //convert m to km
+
+      console.log(
+        "drawing circle at " +
+          e.latlng +
+          " with radius size: " +
+          radiusKM +
+          "km"
+      );
+
+      // //Shawn Code
+      // const newCircle = new LocationCircle(circleCount, e.latlng);
+
+      // newCircle.getCircle.addTo(map);
+
+      // locationCircles.push(newCircle);
+
+      circles[circleCount] = L.circle(e.latlng, {
+        color: "blue",
+        fillColor: "blue",
+        fillOpacity: 0.5,
+        radius: radiusKM,
+      }).addTo(map);
+
+      circleCount++;
+
+      console.log(circles.length);
+      // circle.redraw();
+    } else {
+
+        //POLYGON METHODS
+      console.log(e.latlng);
+
+      if (count == 1) {
+        polyLine = L.polyline([
+          [e.latlng.lat, e.latlng.lng],
+          [e.latlng.lat, e.latlng.lng],
+        ]).addTo(map);
+        count++;
+      } else {
+        polyLine.addLatLng([e.latlng.lat, e.latlng.lng]);
+        count++;
+      }
+    }
+  }
+
+  $("#clearCircle").click(function () {
+    for (i = 0; i < circles.length; i++) {
+      circles[i].remove();
+    }
+
+    i = circles.length;
+
+    while (i !== 0) {
+      circles.pop();
+      i--;
+      console.log(circles);
+    }
+
+    circleCount = 0;
+  });
+
+  $("#drawPolyBtn").click(function () {
+    coords = polyLine.getLatLngs();
+    console.log(coords);
+
+    polygon = L.polygon(coords, {
+      //ill do this later lol
+      color: "red",
+    }).addTo(map);
+
+    setOutput();
+  });
+
+  function setOutput() {
+    //setup input
+    let outputString;
+    outputString = JSON.stringify(polygon.getLatLngs());
+    console.log(JSON.parse(outputString));
+
+    $("#coordOutput").val(outputString);
+  }
+
+  map.on("click", onMapClick);
+
+  $("#clearButton").click(function () {
+    polyLine.setLatLngs([]);
+    clearLine();
+    clearPoly();
+    clearOutput();
+  });
+
+  function clearLine() {
+    polyLine.setLatLngs([]);
+  }
+
+  function clearPoly() {
+    polygon.setLatLngs([]);
+  }
+
+  function clearOutput() {
+    $("#coordOutput").val("");
+  }
+
+
+  //get zips from circles 
+  $("#getCircleZip").click(function(){
     
-    map.on('click', onMapClick);
+   console.log(circles.length);
+   console.log(circles[0]);
+   let output = "Radii: ";
 
-    $("#clearButton").click(function(){ 
-        polyLine.setLatLngs([]);
-        clearLine();
-        clearPoly();
-        clearOutput();
-    });
+   for(i = 0; i < circles.length; i++){
+        output = output + " " + circles[i]._latlng;
+   }
 
-    function clearLine(){ polyLine.setLatLngs([]); }
-    
-    function clearPoly(){ polygon.setLatLngs([]); }
+   $("#coordOutput").val(output);
 
-    function clearOutput(){ $("#coordOutput").val(""); }
-    
+  });
 
-    })//end ready
 
-   
+
+
+}); //end ready
